a81e1c1ddf06efdbafd3dfaacdfe7082abe95730
Curtis Baidoo <jnrcurtis75@gmail.com>
2023-09-24 02:04:47 +0000

    changes made to the database

--- market/__init__.py
+++ market/__init__.py
@@ -4,9 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MarketData6.db'
-
+app.config['SECRET_KEY'] = 'c0f5d23e2ef94421880ad161'
 
 db=SQLAlchemy(app)
-
 
 from market import routes
